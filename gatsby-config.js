4e5ea1703b02e25768c5afedfe85e69f443685f3
duuusen <halloduy@gmail.com>
2019-03-24 20:49:19 +0100

    rewrote header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `Duy Bui — BA IAD Blog`,
+    author: `Duy Bui`,
+    description: `Blog for BA Interaction Design, Zurich University of the Arts, ZHdK`,
+    siteUrl: `https://blog.duy.ch`,
     social: {
-      twitter: `kylemathews`,
+      instagram: `duuusen`,
     },
   },
   plugins: [
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Duy Bui - BA Interaction Design Blog`,
+        short_name: `IAD Blog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "./header"
 import headerStyles from "./header.module.css"
-// import Logo from "../../content/assets/logo.svg"
-
 import { rhythm, scale } from "../utils/typography"
 
 class Layout extends React.Component {
@@ -12,49 +9,20 @@ class Layout extends React.Component {
     const rootPath = `${__PATH_PREFIX__}/`
     let header
     if (location.pathname === rootPath) {
-
   header = (
-    <h1
-      style={{
-        ...scale(1.5),
-        marginBottom: rhythm(1.5),
-        marginTop: 0,
-      }}
-    >
+    <header className={headerStyles.header}>
     <div className={headerStyles.logo}>
       <Link to={'/'} />
     </div>
-      <Link
-        style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-          color: `inherit`,
-        }}
-        to={`/`}
-      >
-        {title}
-      </Link>
-    </h1>
+    </header>
   )
 } else {
   header = (
-    <h3
-      style={{
-        fontFamily: `Montserrat, sans-serif`,
-        marginTop: 0,
-      }}
-    >
-      <Link
-        style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-          color: `inherit`,
-        }}
-        to={`/`}
-      >
-        {title}
-      </Link>
-    </h3>
+    <header className={headerStyles.header}>
+    <div className={headerStyles.logo}>
+      <Link to={'/'} />
+    </div>
+    </header>
   )
 }
     return (
@@ -63,15 +31,15 @@ class Layout extends React.Component {
           marginLeft: `auto`,
           marginRight: `auto`,
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          padding: `${rhythm(1.6)} ${rhythm(3 / 4)}`,
             overrideStyles: ({ adjustFontSizeTo, rhythm }, options, styles) => ({
-                      'h1, h2, h3, h4, h5': {
-                                    color: '#f44242'
-                                }
-                  })
-        }}
+              'h1, h2, h3, h4, h5': {
+                color: '#f44242'
+              }
+            })
+          }}
       >
-        <Header>{header}</Header>
+        {header}
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
